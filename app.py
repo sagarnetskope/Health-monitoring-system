ddade939b7e6ecde66dcf63148ac92a6640bab7b
sagarnetskope <sagar@netskope.com>
2017-04-30 05:56:27 -0700

    Base Celery and flask application

--- app.py
+++ app.py
@@ -281,4 +281,5 @@ def Bmi_calculator(weight,height):
 
 
 if __name__=='__main__':
-    app.run(debug=True,port=9000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
